46bd8b15a511d64d8505cf588080613a145d3123
Kaushal Kumar <kaukumar3@publicisgroupe.net>
2023-06-24 14:46:25 +0530

    now

--- src/main/java/com/java/selfdeveloped/spring/github/action/api/GithubActionsDemoApplication.java
+++ src/main/java/com/java/selfdeveloped/spring/github/action/api/GithubActionsDemoApplication.java
@@ -11,7 +11,7 @@ public class GithubActionsDemoApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Welcome to javatechie !";
+		return "Welcome to javatechie !!";
 	}
 	
 	public static void main(String[] args) {

2219247737b89423fa97ada9c952cc4c87e374b4
Kaushal Kumar <kaukumar3@publicisgroupe.net>
2023-06-24 14:02:38 +0530

    first commit

---
+++ src/test/java/com/java/selfdeveloped/spring/github/action/api/GithubActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.selfdeveloped.spring.github.action.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
